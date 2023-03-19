f80cb731f3db3dbb71dda3d552a553ebed6230c2
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-03-20 00:18:59 +0200

    Refactor read+write code into InputUtils class

--- main.cpp
+++ main.cpp
@@ -7,22 +7,25 @@
 #include <filesystem>
 #include <map>
 
+template <typename Type>
+void prompt_user(std::string const& prompt, std::vector<Type> const& options, std::ostream& output = std::cout) {
+    output << prompt << "\n";
+    for (unsigned int i = 0; i < options.size(); i++) {
+        output << i + 1 << ". ";
+        output << options[i] << "\n";
+    }
+}
+
 template<typename Type>
-std::pair<int, Type> ask_user(std::string const& question, std::vector<Type> const& options, bool number = true) {
+std::pair<int, Type> ask_user(std::string const& question, std::vector<Type> const& options, std::istream& input = std::cin, std::ostream& output = std::cout) {
     while(true) {
-        std::cout << question << "\n";
-        for (unsigned int i = 0; i < options.size(); i++) {
-            if (number) {
-                std::cout << i + 1 << ". ";
-            }
-            std::cout << options[i] << "\n";
-        }
+        prompt_user(question, options, output);
         unsigned int choice;
-        std::cin >> choice;
+        input >> choice;
         if (choice < 1 || choice > options.size()) {
             std::cout << "Invalid choice, try again\n";
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            input.clear();
+            input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             continue;
         }
         return std::make_pair(choice, options[choice - 1]);
@@ -198,8 +201,6 @@ public:
     }
 };
 
-class TaskDataFactory;
-
 class TaskData {
     Heap h;
     std::vector<Teacher> teachers;
@@ -225,116 +226,69 @@ public:
         return h.pop();
     }
 
-    void save_file(const std::filesystem::path& path) {
-        std::ofstream file(path);
-        file<<teachers.size()<<"\n";
-        std::map<std::string, unsigned int> teacher_map;
-        int i = 0;
-        for(const auto& teacher : teachers) {
-            file<<teacher.getName()<<"\n";
-            file<<teacher.getCourse()<<"\n";
-            teacher_map[teacher.getName()] = ++i;
-        }
-        file<<h.size()<<"\n";
-        for(const auto& task : h.getElements()) {
-            file<<teacher_map[task.getTeacher().getName()]<<"\n";
-            file<<task.getPriority()<<"\n";
-            file<<task.getDescription()<<"\n";
-        }
-    }
-
-    friend TaskDataFactory;
+    friend class InputUtils;
 };
 
-class TaskFactory {
+
+class InputUtils {
 public:
-    static Task read_task(std::istream &input, std::vector<Teacher> const &teachers, bool silent){
+    static Task read_task(std::istream &input, std::vector<Teacher> const &teachers, std::ostream &output) {
         std::string description;
         int priority;
         Teacher teacher;
-        if(!silent) {
-            priority = ask_user<std::string>("What is the priority of the task?", Task::getPriorityNames()).first - 1;
-            teacher = ask_user<Teacher>("Which teacher assigned the task?", teachers).second;
-            std::cout << "Enter the description of the task:\n";
-            std::cin >> std::ws;
-            std::getline(std::cin, description);
-        }else{
-            std::string teacher_str;
-            std::string priority_str;
-            std::getline(input, teacher_str);
-            std::getline(input, priority_str);
-            std::getline(input, description);
-            priority = std::stoi(priority_str);
-            teacher = teachers[std::stoi(teacher_str) - 1];
-        }
+        priority = ask_user<std::string>("What is the priority of the task?", Task::getPriorityNames(), input, output).first - 1;
+        teacher = ask_user<Teacher>("Which teacher assigned the task?", teachers, input, output).second;
+        output << "Enter the description of the task:\n";
+        input >> std::ws;
+        std::getline(input, description);
         Task t = Task(teacher, priority, description);
-        if(!silent)
-            std::cout<<t<<"\n";
+        output<<t<<"\n";
         return t;
     }
-};
 
-class TeacherFactory{
-public:
-    static Teacher read_teacher(std::istream &input, bool silent){
-        if(!silent){
-            std::cout << "Enter the name of the teacher:\n";
-            std::string name;
-            std::cin >> std::ws;
-            std::getline(std::cin, name);
-            std::cout << "What does the teacher teach?\n";
-            std::string course;
-            std::cin >> std::ws;
-            std::getline(std::cin, course);
-            return {name, course};
-        } else {
-            std::string name;
-            std::string course;
-            std::getline(input, name);
-            std::getline(input, course);
-            return {name, course};
-        }
+    static Teacher read_teacher(std::istream &input, std::ostream &output) {
+        std::string name, course;
+        output << "Enter the name of the teacher:\n";
+        input >> std::ws;
+        std::getline(input, name);
+        output << "What does the teacher teach?\n";
+        input >> std::ws;
+        std::getline(input, course);
+        return {name, course};
     }
-};
-
-class TaskDataFactory {
-public:
-    static TaskData load_stdin() {
-        std::cout << "How many teachers are there?\n";
-        int teacher_nr;
-        std::cin >> teacher_nr;
+    static TaskData readTaskData(std::istream &input, std::ostream &output, bool assertIsHeap = false) {
+        output<<"Enter the number of teachers:\n";
+        int teacher_nr, task_nr;
+        input>>teacher_nr;
         TaskData td = TaskData();
         for(int i = 0; i < teacher_nr; i++) {
-            td.addTeacher(TeacherFactory::read_teacher(std::cin, false));
+            td.addTeacher(read_teacher(input, output));
         }
-        int task_nr;
-        std::cout << "How many tasks are there?\n";
-        std::cin >> task_nr;
-        for(int i = 0; i < task_nr; i++) {
-            td.addTask(TaskFactory::read_task(std::cin, td.getTeachers(), false));
-        }
-        return td;
-    }
-
-    static TaskData load_file(const std::filesystem::path& path) {
-        std::ifstream file(path);
-        std::string teacher_nr_str;
-        std::getline(file, teacher_nr_str);
-        int teacher_nr = std::stoi(teacher_nr_str);
-        TaskData td = TaskData();
-        for(int i = 0; i < teacher_nr; i++) {
-            td.addTeacher(TeacherFactory::read_teacher(file, true));
-        }
-        std::string task_nr_str;
-        std::getline(file, task_nr_str);
-        int task_nr = std::stoi(task_nr_str);
+        output<<"Enter the number of tasks:\n";
+        input>>task_nr;
         std::vector<Task> tasks;
-        tasks.reserve(task_nr);
-        for(int i = 0; i < task_nr; i++){
-            tasks.push_back(TaskFactory::read_task(file, td.getTeachers(), true));
+        for(int i = 0; i < task_nr; i++) {
+            tasks.push_back(InputUtils::read_task(input, td.getTeachers(), output));
         }
-        td.h = Heap(std::move(tasks), false, true);
+        td.h = Heap(std::move(tasks), false, assertIsHeap);
         return td;
+    }
+    static void writeTaskData(std::ostream &output, const TaskData &td) {
+        output<<td.teachers.size()<<"\n";
+        std::map<std::string, unsigned int> teacher_map;
+        int i = 0;
+        for(const auto& teacher : td.teachers) {
+            output<<teacher.getName()<<"\n";
+            output<<teacher.getCourse()<<"\n";
+            if(teacher_map.find(teacher.getName()) == teacher_map.end())
+                teacher_map[teacher.getName()] = ++i;
+        }
+        output<<td.h.size()<<"\n";
+        for(const auto& task : td.h.getElements()) {
+            output<<task.getPriority() + 1<<"\n";
+            output<<teacher_map[task.getTeacher().getName()]<<"\n";
+            output<<task.getDescription()<<"\n";
+        }
     }
 
 };
@@ -344,13 +298,15 @@ int main(){
     bool option = ask_user<std::string>("Load from file or enter data manually?", {"Load from file", "Enter data manually"}).first == 1;
     TaskData td;
     if(option) {
-        td = TaskDataFactory::load_file(ask_path("Enter the path to load: "));
+        std::ofstream nullOutput{};
+        auto file = std::ifstream(ask_path("Enter the path to load: "));
+        td = InputUtils::readTaskData(file, nullOutput, true);
         if(ask_user<std::string>("Would you like to add more teachers?\n", {"Yes", "No"}).first == 1){
             int teacher_nr;
             std::cout << "How many teachers are there?\n";
             std::cin >> teacher_nr;
             for(int i = 0; i < teacher_nr; i++) {
-                td.addTeacher(TeacherFactory::read_teacher(std::cin, false));
+                td.addTeacher(InputUtils::read_teacher(std::cin, std::cout));
             }
         }
         if(ask_user<std::string>("Would you like to add more tasks to the queue?\n", {"Yes", "No"}).first == 1) {
@@ -358,11 +314,11 @@ int main(){
             std::cout << "How many tasks are there?\n";
             std::cin >> task_nr;
             for(int i = 0; i < task_nr; i++) {
-                td.addTask(TaskFactory::read_task(std::cin, td.getTeachers(), false));
+                td.addTask(InputUtils::read_task(std::cin, td.getTeachers(), std::cout));
             }
         }
     } else {
-        td = TaskDataFactory::load_stdin();
+        td = InputUtils::readTaskData(std::cin, std::cout);
     }
     int query_len;
     std::cout<<"There are "<<td.queueSize()<<" tasks in the queue.\n";
@@ -388,7 +344,8 @@ int main(){
     }
     bool save = ask_user<std::string>("Would you like to save data to file?" + modified_prompt, {"Yes", "No"}).first == 1;
     if (save) {
-        td.save_file(ask_path("Enter the path to save: "));
+        auto file = std::ofstream(ask_path("Enter the path to save: "));
+        InputUtils::writeTaskData(file, td);
     }
     return 0;
 }
