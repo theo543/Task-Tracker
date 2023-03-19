650dc96a8b83c3ab69c372bae0ee8520851b9771
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-03-20 00:21:52 +0200

    Replace pre-increment with two lines
    cppcheck thinks that doesn't need an if statement...
    Also pretty sure this was bugged earlier

--- main.cpp
+++ main.cpp
@@ -276,12 +276,14 @@ public:
     static void writeTaskData(std::ostream &output, const TaskData &td) {
         output<<td.teachers.size()<<"\n";
         std::map<std::string, unsigned int> teacher_map;
-        int i = 0;
+        int next_id = 1;
         for(const auto& teacher : td.teachers) {
             output<<teacher.getName()<<"\n";
             output<<teacher.getCourse()<<"\n";
-            if(teacher_map.find(teacher.getName()) == teacher_map.end())
-                teacher_map[teacher.getName()] = ++i;
+            if(teacher_map.find(teacher.getName()) == teacher_map.end()) {
+                teacher_map[teacher.getName()] = next_id;
+                next_id++;
+            }
         }
         output<<td.h.size()<<"\n";
         for(const auto& task : td.h.getElements()) {
